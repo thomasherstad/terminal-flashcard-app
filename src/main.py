012d2f52b28b301a402978985bc6059797833164
thomasherstad <github@theormsats.fea.st>
2024-07-28 23:53:58 +0200

    Started layout for main menu

--- src/main.py
+++ src/main.py
@@ -1,2 +1,37 @@
-print('hello world')
+from noun import Noun
+from noun_list import NounList
+from db_csv_handler import read_csvfile_to_list,  write_list_to_csvfile
 
+def main_menu():
+    options = {1:'Play',
+               2: 'Add noun',
+               3: 'Search list',
+               4: 'Edit noun',
+               5: 'Close',
+               }
+
+def play(nouns: NounList):
+    pass
+
+
+def add_noun():
+    pass
+
+def search_list():
+    pass
+
+def edit_noun():
+    pass
+
+def close():
+    pass
+
+def main():
+    #1. Load from csv and instantiate a NounList of Nouns
+    #2. Main Menu loop: Give users the options of what to do
+        #3. User chooses
+        #4. Start relevant activity
+    pass
+
+if __name__ == '__main__':
+    main()
